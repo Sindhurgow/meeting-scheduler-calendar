48b8c468bbb76619b625dd2dceb42f88fc0c7dc0
Sindhur H K <hksindhur2808@gmail.com>
2026-02-16 13:48:47 +0530

    Code Updated

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,13 +3,26 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const dbName = process.env.DB_NAME as string;
-const dbUser = process.env.DB_USER as string;
-const dbPassword = process.env.DB_PASSWORD as string;
-const dbHost = process.env.DB_HOST as string;
-const dbDialect = (process.env.DB_DIALECT as any) || 'postgres'; 
-export const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
-    host: dbHost,
-    dialect: dbDialect,
-    logging: false, 
-});
+const databaseUrl = process.env.DATABASE_URL;
+
+export const sequelize = databaseUrl 
+    ? new Sequelize(databaseUrl, {
+        dialect: 'postgres',
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false 
+            }
+        }
+    })
+    : new Sequelize(
+        process.env.DB_NAME as string,
+        process.env.DB_USER as string,
+        process.env.DB_PASSWORD as string,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'postgres',
+            logging: false,
+        }
+    );
